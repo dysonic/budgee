7073b7113575687c1f3ca2c0bfc91650a4896da8
Gareth Dyson <dysonic@rexair.local>
2019-05-10 21:43:29 +1200

    Fix reverse proxy configuration, as the example code I looked at for this was incorrect.

--- reverse-proxy.js
+++ reverse-proxy.js
@@ -1,22 +1,42 @@
 /* eslint-env node */
 const express = require('express');
 const proxy = require('http-proxy-middleware');
-
-// Config
-const { routes } = require('./config.json');
+const router = require('./proxy-router.json');
 
 const app = express();
 
-for (route of routes) {
-    app.use(route.route,
-        proxy({
-            target: route.address,
-            pathRewrite: (path, req) => {
-                return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
-            }
-        })
-    );
-}
+const proxyOptions = {
+    target: 'http://localhost:1000',
+    router,
+    // Could use replace, but take care of the leading '/'
+    // Object.keys(router).forEach(route => path.replace(new RegExp(`^${escapeRegExp(route)}`), ''));
+    pathRewrite: (path, req) => {
+        const targetPath =  '/' + path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
+        console.log('targetPath:', targetPath);
+        return targetPath;
+    },
+};
+
+const apiProxy = proxy('**', proxyOptions);
+app.use(apiProxy);
+
+// let route;
+// for (route of routes) {
+// app.use(route.route,
+//     proxy({
+//         target: route.address,
+//         pathRewrite: (path, req) => {
+//             const targetPath =  path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
+//             console.log('targetPath:', targetPath);
+//             return targetPath;
+//         },
+//         logLevel: 'debug',
+//         // logProvider: (provider) => {
+//         //     // replace the default console log provider.
+//         //     return morgan;
+//         // },
+//     })
+// );
 
 app.listen(8080, () => {
     console.log('Proxy listening on port 8080');
